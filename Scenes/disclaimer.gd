1f356d6eb8064eed3ef3489281583ea806ddafea
BlueGweenie <101449429+BlueGweenie@users.noreply.github.com>
2025-08-05 14:45:28 +0200

    hold on fixed smth

--- Scenes/disclaimer.gd
+++ Scenes/disclaimer.gd
@@ -2,6 +2,5 @@ extends Node2D
 @onready var startscreen = $AudioStreamPlayer
 
 func _input(_event):
-	startscreen.play()
 	if Input.is_action_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
