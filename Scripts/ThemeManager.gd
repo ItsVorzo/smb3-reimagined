d04d5ec8c935cede1ab5360099b58c8dfa353d8f
BlueGweenie <101449429+BlueGweenie@users.noreply.github.com>
2025-08-11 15:27:19 +0200

    stuff

---
+++ Scripts/ThemeManager.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+signal theme_changed(new_theme: String)
+
+@export_enum("Overworld", "Underground", "Desert", "Snow")
+var theme: String = "Overworld":
+	set(value):
+		if theme != value:
+			theme = value
+			emit_signal("theme_changed", value)
+
+func _ready():
+	emit_signal("theme_changed", theme)
