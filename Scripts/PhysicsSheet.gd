73ef041b4ef63a17ef92b835265f0fff4e678419
Trippy <114300595+Trippy-animates@users.noreply.github.com>
2025-08-20 19:01:33 +0200

    more physics stuff

---
+++ Scripts/PhysicsSheet.gd
@@ -0,0 +1,36 @@
+extends Node
+class_name PhysicsVal
+
+# === Flat ground ===
+const acc_speed = [3.28125, 3.28125, 3.28125, 3.28125]
+const frc_speed = [3.28125, 3.28125, 3.28125, 3.28125]
+const skid_speed = [7.5, 7.5, 7.5, 7.5]
+const walk_speed = [90.0, 90.0, 90.0, 90.0]
+const run_speed = [150.0, 150.0, 150.0, 150.0]
+const p_speed = [210.0, 210.0, 210.0, 210.0]
+const end_level_walk = 75.0
+const airship_cutscene_walk = 120.0
+
+# === Slopes ===
+const uphill_max_walk = [48.75, 48.75, 48.75, 48.75]
+const uphill_max_run = [82.5, 82.5, 82.5, 82.5]
+const sliding_max_speed = 236.25
+
+# === Mid-air ===
+const jump_speeds = [-217.5, -225.0, -240, -258.75,
+					-217.5, -225.0, -240, -258.75,
+					-217.5, -225.0, -240, -258.75,
+					-217.5, -225.0, -240, -258.75
+					]
+
+const low_gravity = [256.0, 256.0, 256.0, 256.0]
+const high_gravity = [1280.0, 1280.0, 1280.0, 1280.0]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,34 +1,43 @@
 extends CharacterBody2D
 class_name Player
 
-# === Tunable Constants ===
-const walk_speed = 90.0
-const run_speed = 150.0
-const p_speed = 210.0
-const acc_speed = 3.28125
-const frc_speed = 3.28125
-const skid_speed = 7.5
-
-const jump_speeds = [-217.5, -225.0, -240, -258.75]
-
-const coyote_time = 0.1
-const jump_buffer_time = 0.1
-
-var p_meter = 0.0
-var p_meter_max = 70.0
-var previous_max_speed = walk_speed
-var max_speed = walk_speed
-var grav_speed = 800.0
-var velocity_direction = sign(velocity.x)
-
+# === Shortcuts ===
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump = $AudioStreamPlayer2D
 @onready var normal_collision_shape := $CollisionShape2D
 @onready var super_collision_shape := $SuperCollisionShape2D
 @onready var death_sound: AudioStreamPlayer2D = $DeathSoundPlayer
 @onready var bottom_pit := $"../CameraGroundLimit"
+var character_index = SaveManager.runtime_data.get("character_index", 0)
 
-# === State ===
+# === Physics values ===
+var walk_speed = PhysicsVal.walk_speed[character_index]
+var run_speed = PhysicsVal.run_speed[character_index]
+var p_speed = PhysicsVal.p_speed[character_index]
+var acc_speed = PhysicsVal.acc_speed[character_index]
+var frc_speed = PhysicsVal.frc_speed[character_index]
+var skid_speed = PhysicsVal.skid_speed[character_index]
+var end_level_walk = PhysicsVal.end_level_walk
+var airship_cutscene_walk = PhysicsVal.airship_cutscene_walk
+
+var jump_speeds = PhysicsVal.jump_speeds.slice(character_index * 4, 4)
+
+var low_gravity = PhysicsVal.low_gravity[character_index]
+var high_gravity = PhysicsVal.high_gravity[character_index]
+const death_gravity = 800.0
+
+# === Other stuff ===
+const coyote_time = 0.1
+const jump_buffer_time = 0.1
+
+var p_meter = 0.0
+var p_meter_max = 70.0
+var extra_p_frames = 16.0
+var previous_max_speed = walk_speed
+var max_speed = walk_speed
+var velocity_direction: int
+
+# === States ===
 var jump_buffer_timer = 0.0
 var coyote_timer = 0.0
 var is_skidding = false
@@ -48,6 +57,8 @@ func _process(delta):
 	elif InputManager.direction != 0:
 		facing_direction = InputManager.direction
 	animated_sprite.scale.x = facing_direction
+
+	velocity_direction = sign(velocity.x)
 
 	# === Timers ===
 	if not is_on_floor():
@@ -76,9 +87,11 @@ func _process(delta):
 
 	# === P meter ===
 	p_meter = clamp(p_meter, 0, p_meter_max)
+	if p_meter >= p_meter_max and InputManager.B and InputManager.direction == velocity_direction and abs(velocity.x) >= run_speed: extra_p_frames = 16.0
+	elif extra_p_frames > 0: extra_p_frames -= 1
 	if abs(velocity.x) >= run_speed and InputManager.B and is_on_floor() or not is_on_floor() and p_meter >= p_meter_max:
 		p_meter += 1
-	elif p_meter > 0:
+	elif p_meter > 0 and extra_p_frames <= 0:
 		p_meter -= 0.583
 
 func power_up():
@@ -113,7 +126,7 @@ func _physics_process(delta: float) -> void:
 			"jump", "fall":
 				animated_sprite.play("dead")
 				# custom gravity; no normal physics while dead
-				velocity.y += grav_speed * delta
+				velocity.y += death_gravity * delta
 				move_and_slide()
 		return
 	# ======= END DEATH OVERRIDE =======
@@ -166,10 +179,10 @@ func _physics_process(delta: float) -> void:
 	# === Gravity and Jumping ===
 	var final_grav_speed: float
 	if not is_on_floor():
-		if velocity.y < -120 and InputManager.A: final_grav_speed = 256.0
-		else: final_grav_speed = 1280.0
+		if velocity.y < -120 and InputManager.A: final_grav_speed = low_gravity
+		else: final_grav_speed = high_gravity
 	if not is_on_floor(): velocity.y += final_grav_speed * delta
-	velocity.y = min(velocity.y, 240)
+	velocity.y = min(velocity.y, 258.75)
 
 	# === Jumping ===
 	if InputManager.Apress and jump_buffer_timer > 0.0 and coyote_timer > 0.0 or InputManager.Apress and is_on_floor():
