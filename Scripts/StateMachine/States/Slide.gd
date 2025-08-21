aa2c7b9a31fe57d48d1fdc6b92d3a61850be9130
Trippy <114300595+Trippy-animates@users.noreply.github.com>
2025-08-21 20:57:46 +0200

    slide fix attempt

--- Scripts/StateMachine/States/Slide.gd
+++ Scripts/StateMachine/States/Slide.gd
@@ -6,8 +6,17 @@ func enter() -> void:
 
 
 func physics_process_update(_delta: float) -> void:
-	if InputManager.direction != 0 or (player.get_slope_angle() == 0 and player.velocity.x == 0):
+	if player.get_slope_angle() == 0:
+		if abs(player.velocity.x) < 10:
+			state_machine.change_state("Normal")
+		if InputManager.direction == player.velocity_direction:
+			state_machine.change_state("Normal")
+	if InputManager.direction != player.velocity_direction and InputManager.direction != 0:
 		state_machine.change_state("Normal")
 	if player.velocity_direction != 0: 
 		player.facing_direction = player.velocity_direction
+	print(player.max_speed, " + ", player.final_acc_speed())
 	player.velocity.x = move_toward(player.velocity.x, player.max_speed * player.get_slope_direction(), player.final_acc_speed())
+
+func exit() -> void:
+	pass
