cd3ec6434e8abb95405c5bb6c6146e9365509dfd
ItsVorzo <167795697+ItsVorzo@users.noreply.github.com>
2025-08-28 19:46:34 +0100

    shell shit

--- Scripts/GrabbableClass.gd
+++ Scripts/GrabbableClass.gd
@@ -3,9 +3,10 @@ extends Node
 
 @export var grabbox: Area2D = null
 var velocity: Vector2 = Vector2.ZERO
-var plr: Player
+var plr: Player = null
 var is_grabbed := false
 var is_kicked := false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,14 +14,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	is_kicked = not is_grabbed
 	var bodies = grabbox.get_overlapping_bodies()
+	var grabbed_this_frame = false
+
 	for body in bodies:
 		if body.is_in_group("Player"):
-			is_grabbed = body.input.is_action_pressed("B")
-			if is_grabbed:
+			if body.input.is_action_pressed("B"):
+				is_grabbed = true
+				plr = body
+				grabbed_this_frame = true
 				owner.global_position.x = body.global_position.x + 10 * body.facing_direction
 				owner.global_position.y = body.global_position.y - 16
 				is_kicked = false
-			else:
-				is_kicked = true
+				break
+
+	if not grabbed_this_frame:
+		is_grabbed = false
+		plr = null
+		is_kicked = true
