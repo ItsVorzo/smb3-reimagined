33b94144b9dc442fa12ff025bfda5d347604316a
ItsVorzo <167795697+ItsVorzo@users.noreply.github.com>
2025-08-29 01:49:51 +0100

    commit

--- Scripts/Enemies/KoopaShell.gd
+++ Scripts/Enemies/KoopaShell.gd
@@ -2,16 +2,34 @@ extends CharacterBody2D
 
 @onready var grab = $Grabbable
 var gravity = 500.0
+var bounce_force = -80.0  # Smaller bounce
+var has_bounced = false
+var was_on_floor = false
+var was_grabbed = false  # New: track previous grab state
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if not grab.is_grabbed:
+	var is_currently_grabbed = grab.is_grabbed
+
+	if not is_currently_grabbed:
 		velocity.y += gravity * delta
-	else: velocity.y = 0
+
+		# Allow bounce if just released and hits the floor
+		if is_on_floor() and not was_on_floor and not has_bounced and velocity.y > 0:
+			velocity.y = bounce_force
+			has_bounced = true
+	else:
+		# While being held
+		velocity.y = 0
+
+	# Reset bounce if it was just released
+	if was_grabbed and not is_currently_grabbed:
+		has_bounced = false
+
+	# Update state trackers
+	was_on_floor = is_on_floor()
+	was_grabbed = is_currently_grabbed
 
 	move_and_slide()
--- Scripts/Game/GameManager.gd
+++ Scripts/Game/GameManager.gd
@@ -5,7 +5,7 @@ var cam: Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
