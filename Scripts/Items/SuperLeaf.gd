bcd7e73f5533eaf3f30edf7f3c56ecd82c440427
Trippy <114300595+Trippy-animates@users.noreply.github.com>
2025-12-10 18:44:40 +0100

    super leaf item movement

--- Scripts/Items/SuperLeaf.gd
+++ Scripts/Items/SuperLeaf.gd
@@ -1,26 +1,44 @@
 extends PowerUpItem
 
 var start_x
-var timer := 0.0
-var fall_speed := 28.0
+var x_speed := 0.0
+var swinging_direction := 1
+var acc_speed := 7.6
+var max_speed := 120.0
+var gravity := 500.0
 
 func _ready() -> void:
 	super._ready()
 	start_x = global_position.x
 	if from_block:
-		velocity.y = -400.0
+		x_speed = 0.0
+		velocity.y = -200.0
 
 func _physics_process(delta: float) -> void:
 	
 	if from_block:
-		if velocity.y == 0:
+		velocity.y += gravity * delta
+		move_and_slide()
+		if velocity.y >= 0.0:
 			from_block = false
 		return
 
-	$AnimatedSprite2D.scale.x = 1.0 * direction
+	# Leaf oscillation (THANKS WYE XOXO)
+	x_speed += acc_speed * swinging_direction
 
-	timer += delta
-	velocity.x = start_x + 64 * (sin(timer * 4))
-	velocity.y = sin(timer * 8.0) * 19.0 + 9.0
+	if abs(x_speed) >= max_speed:
+		swinging_direction *= -1
+		x_speed = max_speed * sign(x_speed)
 
-	#move_and_slide()
+	velocity.x = x_speed
+
+	if swinging_direction != sign(x_speed):
+		velocity.y = -15.0
+	elif swinging_direction == -1:
+		velocity.y = 52.5
+	else:
+		velocity.y = 60.0
+
+	$AnimatedSprite2D.flip_h = x_speed > 0
+
+	move_and_slide()
